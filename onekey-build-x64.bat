4d35446a2cef75ae7080ce4f4b4af9267d74a1a6
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-12-09 23:02:55 +0800

    Adjust base compile script.

--- onekey-build-x64.bat
+++ onekey-build-x64.bat
@@ -22,13 +22,13 @@ mkdir %BinaryFolder%
 
 set PATH=%~dp0qtbase\bin;%PATH%
 
-set CommonOptions=-DBUILD_SHARED_LIBS=ON -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON -DQT_BUILD_EXAMPLES=OFF -DQT_BUILD_TESTS=OFF
+set CommonOptions=-G "Ninja Multi-Config" -DCMAKE_CONFIGURATION_TYPES=Release;Debug -DBUILD_SHARED_LIBS=ON -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON -DQT_BUILD_EXAMPLES=OFF -DQT_BUILD_TESTS=OFF
 
 mkdir %ObjectFolder%\qtbase
 pushd %ObjectFolder%\qtbase
-cmake -DCMAKE_INSTALL_PREFIX=%BinaryFolder% %CommonOptions% -G "Ninja Multi-Config" -DCMAKE_CONFIGURATION_TYPES=Release;Debug -DFEATURE_optimize_size=ON -DINPUT_openssl=no -DFEATURE_schannel=ON -DINPUT_opengl=no -DFEATURE_dbus=OFF -DFEATURE_freetype=OFF -DFEATURE_system_freetype=OFF -DFEATURE_harfbuzz=OFF -DFEATURE_system_harfbuzz=OFF -DFEATURE_system_zlib=OFF ../../../../qtbase
+cmake -DCMAKE_INSTALL_PREFIX=%BinaryFolder% %CommonOptions% -DFEATURE_optimize_size=ON -DINPUT_openssl=no -DFEATURE_schannel=ON -DINPUT_opengl=no -DFEATURE_dbus=OFF -DFEATURE_freetype=OFF -DFEATURE_system_freetype=OFF -DFEATURE_harfbuzz=OFF -DFEATURE_system_harfbuzz=OFF -DFEATURE_system_zlib=OFF ../../../../qtbase
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
-ninja
+cmake --build . --parallel
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 ninja install
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
