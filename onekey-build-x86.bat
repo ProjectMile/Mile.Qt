46c6ed3bc2670955b9e9017b31c30c9e8b608818
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-12-11 06:46:57 +0800

    Add compile script for qttools.

--- onekey-build-x86.bat
+++ onekey-build-x86.bat
@@ -66,4 +66,14 @@ ninja install
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 popd
 
+mkdir %ObjectFolder%\qttools
+pushd %ObjectFolder%\qttools
+cmake -DCMAKE_PREFIX_PATH=%BinaryFolder% -DCMAKE_INSTALL_PREFIX=%BinaryFolder% %CommonOptions% ../../../../qttools
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+cmake --build . --parallel
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+ninja install
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+popd
+
 @endlocal
