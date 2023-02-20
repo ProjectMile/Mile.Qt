8d924ecef456bc5c12d5a606bc438d352e5a1a32
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-02-20 09:18:52 +0800

    Remove mourinaruto-wangwenx190-qtbase-target-tweak.patch.

--- apply-patches.bat
+++ apply-patches.bat
@@ -8,8 +8,6 @@ set QtBaseFolder="%~dp0qtbase"
 pushd %QtBaseFolder%
 git am %PatchesFolder%\wangwenx190-qtbase-tweaks.patch
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
-git am %PatchesFolder%\mourinaruto-wangwenx190-qtbase-target-tweak.patch
-if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 popd
 
 @endlocal
